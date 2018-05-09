2643ccc03467b1a5d896b4c3e032b80c4e5c3403
glebtv <glebtv@gmail.com>
2018-05-09 10:22:19 +0300

    fixes

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,5 +1,8 @@
 import { configure } from '@storybook/react';
 
+import 'bootstrap/dist/css/bootstrap.css';
+import 'react-select/dist/react-select.min.css';
+
 // automatically import all files ending in *.stories.js
 const req = require.context('../stories', true, /.stories.js$/);
 function loadStories() {
